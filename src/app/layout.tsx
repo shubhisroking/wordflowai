0e3c7967edfa855b2f608e9d0993247b154d9bf1
Shubh <shubhisroking@proton.me>
2025-09-04 21:48:02 +0530

    force light theme
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,8 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-          enableSystem
+          enableSystem={false}
+          forcedTheme="light"
           disableTransitionOnChange
         >
           {children}
